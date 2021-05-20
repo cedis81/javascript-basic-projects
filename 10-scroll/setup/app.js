e0b2764b2e40ed4e8bf3f1e9b3d03a679c5b1ec0
cedis81 <ccchom11@gmail.com>
2021-05-20 18:14:39 -0400

    Complete basic scroll project
    * Target scroll-link classes in app.js
    * Add smooth scroll function to calculate height of each section
    * Scroll to section based on navbar height
    * Close navbar when scrolled on smaller screen

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -48,3 +48,34 @@ window.addEventListener('scroll', function() {
 })
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    // prevent default
+    event.preventDefault();
+    // navigate to specific location
+    const id = event.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    // calculate heights
+
+    const navHeight = navBar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navBar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left:0,
+      top: position,
+    })
+    linksContainer.style.height = 0;
+  })
+})
